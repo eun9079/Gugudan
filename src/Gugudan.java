0298c93b0d2c71dfc996bfedd011838302b11045
eunn9 <eunn9@DESKTOP-JHF7QR1>
2018-11-06 19:49:09 +0900

    4단 delete

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -23,16 +23,6 @@ public class Gugudan {
 		System.out.println(3 * 7);
 		System.out.println(3 * 8);
 		System.out.println(3 * 9);
-		// 4단
-		System.out.println("4단");
-		System.out.println(4 * 1);
-		System.out.println(4 * 2);
-		System.out.println(4 * 3);
-		System.out.println(4 * 4);
-		System.out.println(4 * 5);
-		System.out.println(4 * 6);
-		System.out.println(4 * 7);
-		System.out.println(4 * 8);
-		System.out.println(4 * 9);
+	
 }
 }
